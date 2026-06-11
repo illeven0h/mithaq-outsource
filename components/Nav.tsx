f503544f1ab2ce688a9a18d3fedbb4ff75eaa67c
Iman Mughal <imanmughal1060@gmail.com>
2026-06-11 17:27:07 +0500

    removed errors

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,9 +2,6 @@
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { FinalCTA } from '/components/Sections'
-
-
 
 const navLinks = [
   ['Services', '#services'],
